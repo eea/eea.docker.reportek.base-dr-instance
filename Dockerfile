cd510e582ac22cbef2168217673f9301cc33e8fc
Olimpiu Rob <olimpiu.rob@gmail.com>
2019-05-10 17:05:26 +0300

    Refs #105518; Added cron support

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,15 @@ ENV LOCAL_CONVERTERS_HOST=converter
 USER root
 COPY src/*                      $ZOPE_HOME/
 COPY zope-setup.sh              \
+     docker-entrypoint.sh       \
      docker-initialize.py       /
 
 RUN buildDeps="gcc" \
- && runDeps="gosu libjpeg62 libopenjp2-7 libtiff5 libxml2 libxslt1.1 lynx netcat poppler-utils rsync wv git-core libsasl2-dev python-dev libldap2-dev libssl-dev curl iputils-ping iproute2 vim" \
+ && runDeps="gosu libjpeg62 libopenjp2-7 libtiff5 libxml2 libxslt1.1 lynx netcat poppler-utils rsync wv git-core libsasl2-dev python-dev libldap2-dev libssl-dev curl iputils-ping iproute2 vim cron netcat-openbsd sudo procps" \
  && apt-get update \
  && apt-get install -y --no-install-recommends $buildDeps \
  && apt-get install -y --no-install-recommends $runDeps \
+ && echo "zope-www ALL = NOPASSWD: /etc/init.d/cron"  > /etc/sudoers \
  && pip install python-ldap==2.4.38 \
  && cd $ZOPE_HOME && ./install.sh \
  && chown -R 500:500 $ZOPE_HOME \
---
+++ docker-entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+COMMANDS="debug help logtail show stop adduser fg kill quit run wait console foreground logreopen reload shell status"
+CRONJOB="cronjob"
+START="start restart"
+CMD="bin/instance"
+SETUPCMD="/zope-setup.sh"
+
+$SETUPCMD
+
+if [ -z "$HEALTH_CHECK_TIMEOUT" ]; then
+  HEALTH_CHECK_TIMEOUT=1
+fi
+
+if [ -z "$HEALTH_CHECK_INTERVAL" ]; then
+  HEALTH_CHECK_INTERVAL=1
+fi
+
+if [[ $START == *"$1"* ]]; then
+  if [ ! -z $DEBUG ]; then
+    if [[ $DEBUG == *on* ]]; then
+      exec $CMD fg
+    fi
+  else
+    _stop() {
+      $CMD stop
+      kill -TERM $child 2>/dev/null
+    }
+
+    trap _stop SIGTERM SIGINT
+    $CMD start
+
+    child=$!
+    pid=`$CMD status | sed 's/[^0-9]*//g'`
+    if [ ! -z "$pid" ]; then
+      echo "Application running on pid=$pid"
+      sleep "$HEALTH_CHECK_TIMEOUT"
+      while kill -0 "$pid" 2> /dev/null; do
+        sleep "$HEALTH_CHECK_INTERVAL"
+      done
+    else
+      echo "Application didn't start normally. Shutting down!"
+      _stop
+    fi
+  fi
+else
+  if [[ $COMMANDS == *"$1"* ]]; then
+    exec $CMD "$@"
+  elif [[ $CRONJOB == *"$1"* && ! -z $CRONTAB ]]; then
+    echo "$CRONTAB" > /tmp/crontab
+    # start netcat to keep the healthchecker happy
+    nc -lkp 8080 &>/dev/null &
+    crontab /tmp/crontab
+    sudo /etc/init.d/cron start
+    tail -f /dev/null
+  else
+    exec "$@"
+  fi
+fi
